18ec91bbe61f3674c73c7ceabfa85c2cd5770037
suwings <suwings@outlook.com>
2017-11-18 15:48:48 +0800

    rewrite packeage.json

--- node_modules/vue/src/compiler/directives/index.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import bind from './bind'
-import { noop } from 'shared/util'
-
-export default {
-  bind,
-  cloak: noop
-}
--- node_modules/vue/src/compiler/parser/entity-decoder.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-let decoder
-
-export function decode (html: string): string {
-  decoder = decoder || document.createElement('div')
-  decoder.innerHTML = html
-  return decoder.textContent
-}
