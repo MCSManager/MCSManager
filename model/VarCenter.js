14bd665920c6b4b864f4fdfa4ee4ff71c5e37ec3
suwings <suwings@outlook.com>
2018-04-02 23:39:56 +0800

    重建 VarCenter

--- model/VarCenter.js
+++ model/VarCenter.js
@@ -1,11 +1,12 @@
-var _VAR_LIST = {};
+//全局用户 token 保存区
+global.__MCSERVER_VAR_LIST__ = {};
 
 
 module.exports.set = (key, value) => {
-    _VAR_LIST[key] = value;
+    global.__MCSERVER_VAR_LIST__[key] = value;
 }
 
 
 module.exports.get = (key, def = undefined) => {
-    return _VAR_LIST[key] || def;
+    return global.__MCSERVER_VAR_LIST__[key] || def;
 }
