fcb66cf8c8f48c26b874d7107ef32afae26f272a
suwings <suwings@outlook.com>
2018-04-13 09:55:43 +0800

    整理 - 删除老注释

--- model/WebSocketModel.js
+++ model/WebSocketModel.js
@@ -1,11 +1,5 @@
 const EventEmitter = require('events');
 const observerModel = require('./ObserverModel');
-// class WebSocketObserver extends EventEmitter {
-
-// }
-
-
-// var onlyWebSocketObserver = new WebSocketObserver();
 
 //事件二次转发  监听ws/req即可监听所有Websocket请求
 observerModel.listener('ws/req', '', (data) => {
