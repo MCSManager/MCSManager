9865eea11c51218ac4fa3709ad48c126490524d9
suwings <suwings@outlook.com>
2018-04-29 11:36:12 +0800

    修改 - URL 定位器

--- public/common/URL.js
+++ public/common/URL.js
@@ -2,13 +2,24 @@
 //如果你的程序不在根目录，可以考虑更改这里，或者你有什么其他姿势。
 //如果你需要反向代理加入SSL，请更改此处
 
+//某些 login 页面没有 MCSERVER 全局变量，在此实例化
+if (window.MCSERVER == undefined) window.MCSERVER = {};
+
+
 //Ws 默认协议
 MCSERVER.WS_PROTOCOL = 'ws://';
 //HTTP 默认协议
 MCSERVER.HTTP_PROTOCOL = 'http://';
 
-//某些 login 页面没有 MCSERVER 全局变量，在此实例化
-if (window.MCSERVER == undefined) window.MCSERVER = {};
+
+console.log([
+	'PROTOCOL: ',
+	'WS_PROTOCOL:',
+	MCSERVER.WS_PROTOCOL,
+	'| HTTP_PROTOCOL:',
+	MCSERVER.HTTP_PROTOCOL
+].join(" "));
+
 
 //URL定位器
 MCSERVER.URL = function (url, protocol) {
