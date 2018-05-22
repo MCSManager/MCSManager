8296fa1119ad6e95f2abf6c74a7602d7942dd0f9
suwings <suwings@outlook.com>
2018-05-22 21:20:54 +0800

    修改 - 控制台色彩初步优化

--- public/common/js/tools.js
+++ public/common/js/tools.js
@@ -115,22 +115,25 @@
 		text = text.replace(/INFO/gm, "<span style='color:#03ea0a;'>INFO</span>");
 		text = text.replace(/WANG/gm, "<span class='color-red'>WANG</span>");
 		text = text.replace(/(\d{2,}:\d{2,}:\d{2,})/gm, "<span style='color:#017EBC;'> $1 </span>");
+
 		RegExpStringArr = [
-			["Unknown&nbsp;command", "Server", "Stopping"],
+			["Unknown command", "Stopping", "Loading libraries, please wait..."],
 			["/help"],
-			["WARN"]
+			["WARN", "EULA"]
 		]
 		for (var k in RegExpStringArr) {
 			for (var y in RegExpStringArr[k]) {
 
-				var reg = new RegExp("(" + RegExpStringArr[k][y] + ")", "igm");
+				var reg = new RegExp("(" + RegExpStringArr[k][y].replace(/ /igm, "&nbsp;") + ")", "igm");
 				console.log("当前:", k, y, "正则是:", "(" + RegExpStringArr[k][y] + ")", reg)
 				if (k == 0)
 					text = text.replace(reg, "<span style='color:#017EBC;'>$1</span>");
 				if (k == 1)
 					text = text.replace(reg, "<span class='color-green'>$1</span>");
 				if (k == 2)
-					text = text.replace(reg, "<span class='color-red'>$1</span>");
+					text = text.replace(reg, "<span style='color:#ea1f1a;'>$1</span>");
+
+
 			}
 		}
 		return text;
