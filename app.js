2fd28a2f0349d79e481d154731a456c1ce71a7a4
suwings <suwings@outlook.com>
2018-05-30 17:03:08 +0800

    修改 - 版本最低限制

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //运行时环境检测
 try {
     let versionNum = parseInt(process.version.replace(/v/igm, "").split(".")[0]);
-    if (versionNum < 8) {
-        console.log("[ ERROR ] 您的 Node 运行环境版本似乎低于我们要求的版本.");
-        console.log("[ ERROR ] 可能会出现未知异常,请立即更新 Node 版本 (>=8.0.0)");
+    if (versionNum < 7) {
+        console.log("[ WARN ] 您的 Node 运行环境版本似乎低于我们要求的版本.");
+        console.log("[ WARN ] 可能会出现未知情况,建议您更新 Node 版本 (>=7.0.0)");
     }
 } catch (err) {
     //忽略任何版本检测导致的错误
 }
 
+//测试
 setTimeout(() => {
     let arg2 = process.argv[2] || '';
     if (arg2 == '--test') {
