b5f8e43c6680346d8e44d5db8e8aeb2a2995aab4
Suwings <suwings@outlook.com>
2019-10-31 12:45:03 +0800

    修改 错误路由文字描述

--- route/error.js
+++ route/error.js
@@ -2,31 +2,31 @@
 const router = require('express')();
 
 //路由定义
-router.get('/404', function(req, res) {
+router.get('/404', function (req, res) {
     res.send('404 地址错误');
 });
 
-router.get('/500', function(req, res) {
+router.get('/500', function (req, res) {
     res.send('500 服务器错误');
 });
 
-router.get('/403', function(req, res) {
+router.get('/403', function (req, res) {
     res.send('403 权限不足');
 });
 
-router.get('/599', function(req, res) {
+router.get('/599', function (req, res) {
     res.send('599 不可思议的操作');
 });
 
-router.get('/499', function(req, res) {
+router.get('/499', function (req, res) {
     res.send('499 安全机制触发');
 });
 
-router.get('/notlogin', function(req, res) {
-    res.send('你特么不登陆么？');
+router.get('/notlogin', function (req, res) {
+    res.send('您没有登录');
 });
 
-router.get('/token', function(req, res) {
+router.get('/token', function (req, res) {
     res.send('token ERROR');
 });
 
