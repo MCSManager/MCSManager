dcf9b4cf92d083817710d9e24e8ce544a8dfadfb
Suwings <suwings@outlook.com>
2020-01-26 19:13:10 +0800

    新增 访问API

--- route/api.js
+++ route/api.js
@@ -1,9 +1,10 @@
 const router = require('express')();
 const serverModel = require('../model/ServerModel');
 const fs = require('fs');
+const mcPingProtocol = require('../helper/MCPingProtocol');
 
 //公开 服务端状态获取 JSON格式
-router.all('/get/:name', function (req, res) {
+router.all('/status/:name', function (req, res) {
     if (MCSERVER.localProperty.allow_status_api) {
         res.send("管理员禁止此项功能 | Access denied");
         return;
@@ -15,43 +16,63 @@ router.all('/get/:name', function (req, res) {
         res.send("null");
         return;
     }
-    let sendStatus = {
-        serverName: serverName,
-        lastDate: mcserver.dataModel.lastDate,
-        status: mcserver.isRun()
-    };
+    let sendStatus = null;
+
+    // 取缓存资料
+    const mcpingResult = mcPingProtocol.QueryMCPingTask(serverName)
+
+    // 判断服务器启动状态发送不同的数据
+    if (mcserver.isRun() && mcpingResult) {
+        sendStatus = {
+            id: serverName,
+            serverName: mcserver.dataModel.mcpingConfig.mcpingName,
+            lastDate: mcserver.dataModel.mcpingConfig.lastDate,
+            status: mcserver.isRun(),
+            current_players: mcpingResult.current_players,
+            max_players: mcpingResult.max_players,
+            motd: mcserver.dataModel.mcpingConfig.mcpingMotd || mcpingResult.motd,
+            version: mcpingResult.version
+        };
+    } else {
+        sendStatus = {
+            id: serverName,
+            lastDate: mcserver.dataModel.lastDate,
+            status: mcserver.isRun(),
+        };
+    }
+
     res.send(JSON.stringify(sendStatus));
     res.end();
 });
 
 
 //公开 服务端状态获取 可视模式
-router.all('/status/:name', function (req, res) {
-    if (MCSERVER.localProperty.allow_status_api) {
-        res.send("管理员禁止此项功能 | Access denied");
-        return;
-    }
-    let params = req.params || {};
-    let serverName = params.name || "";
-    //十分简单的网页模板
-    try {
-        let template = fs.readFileSync("./public/template/api_server.html").toString();
-        let mcserver = serverModel.ServerManager().getServer(serverName.trim());
-        if (mcserver == null) {
-            res.send("Not Found");
-            return;
-        }
-        //渲染模板
-        template = template.replace(/\$servername/igm, serverName);
-        template = template.replace(/\$last_time/igm, mcserver.dataModel.lastDate);
-        template = template.replace(/\$status/igm, mcserver.isRun() ? "正在运行" : "关闭");
+// router.all('/status/:name', function (req, res) {
+//     if (MCSERVER.localProperty.allow_status_api) {
+//         res.send("管理员禁止此项功能 | Access denied");
+//         return;
+//     }
+//     let params = req.params || {};
+//     let serverName = params.name || "";
+//     //十分简单的网页模板
+//     try {
+//         let template = fs.readFileSync("./public/template/api_server.html").toString();
+//         let mcserver = serverModel.ServerManager().getServer(serverName.trim());
+//         if (mcserver == null) {
+//             res.send("Not Found");
+//             return;
+//         }
+//         //渲染模板
+//         template = template.replace(/\$servername/igm, serverName);
+//         template = template.replace(/\$last_time/igm, mcserver.dataModel.lastDate);
+//         template = template.replace(/\$status/igm, mcserver.isRun() ? "正在运行" : "关闭");
 
-        res.send(template);
-    } catch (err) {
-        MCSERVER.error('API 处理异常:', err);
-    }
-    res.end();
-});
+//         res.send(template);
+//     } catch (err) {
+//         MCSERVER.error('API 处理异常:', err);
+//     }
+//     res.end();
+// });
 
 
 //模块导出
