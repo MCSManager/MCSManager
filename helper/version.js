7fc33ae09050c5bf10aa0744aa482d1e8071ad46
Suwings <suwings@outlook.com>
2020-03-29 20:05:01 +0800

    修改 版本编号

--- helper/version.js
+++ helper/version.js
@@ -7,7 +7,7 @@ const os = require("os");
 
 const verisonA = "8.6.8 发行版本"; //发行版本
 
-const verisonB = "No.2020031123"; //版本批次
+const verisonB = "No.2020032920"; //版本批次
 
 let info = [os.type(), os.arch(), os.hostname(), os.release()].join(" ");
 
