613a44da4b4d306c654614436597d3806a3ae2d9
huangsijun17 <huangsijun17@hotmail.com>
2020-08-31 20:11:55 +0800

    修复服务器列表的人数显示

--- helper/MCPingProtocol.js
+++ helper/MCPingProtocol.js
@@ -43,7 +43,7 @@ class MCServStatus {
             // });
 
             client.on('data', function(data){
-                console.log(data.toString().substr(3,));
+                // console.log(data.toString().substr(3,));
                 resolve(data.toString().substr(3,));
             });
            
