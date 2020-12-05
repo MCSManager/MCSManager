5e9266ca8c614b891cd13ee8e12faee73230b58f
Suwings <Suwings@outlook.com>
2020-12-05 23:38:55 +0800

    修改 发行版本

--- helper/version.js
+++ helper/version.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: Copyright(c) 2020 Suwings
+ * @Date: 2020-10-08 13:28:28
+ * @LastEditTime: 2020-12-05 23:38:41
+ * @Description: 版本发行说明文件
+ */
 const os = require("os");
 
 //前端显示版本
@@ -5,9 +11,9 @@ const os = require("os");
 //这样, 用户截图时, 可以知道具体的版本
 //请用户尽可能的不要修改本文件任何代码，因为每一次版本更新时，必定会冲突
 
-const verisonA = "8.6.15 发行版本"; //发行版本
+const verisonA = "8.6.16 发行版本"; //发行版本
 
-const verisonB = "No.20201024-06"; //版本批次
+const verisonB = "No.20201205-01"; //版本批次
 
 let info = [os.type(), os.arch(), os.hostname(), os.release()].join(" ");
 
