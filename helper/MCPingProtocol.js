da296df59e12f65ed34fe74390b7667d13642b45
Suwings <Suwings@outlook.com>
2021-11-10 16:20:15 +0800

    整理 格式化代码

--- helper/MCPingProtocol.js
+++ helper/MCPingProtocol.js
@@ -7,84 +7,84 @@ const net = require("net");
 // Github: https://github.com/Vaksty/mcping
 
 class MCServStatus {
-    constructor(port, host) {
-        this.port = port;
-        this.host = host;
+  constructor(port, host) {
+    this.port = port;
+    this.host = host;
+    this.status = {
+      online: null,
+      version: null,
+      motd: null,
+      current_players: null,
+      max_players: null,
+      latency: null
+    };
+  }
+
+  getStatus() {
+    return new Promise((resolve, reject) => {
+      var start_time = new Date();
+      const client = net.connect(this.port, this.host, () => {
+        this.status.latency = Math.round(new Date() - start_time);
+        // 0xFE packet identifier for a server list ping
+        // 0x01 server list ping's payload (always 1)
+        let data = Buffer.from([0xfe, 0x01]);
+        client.write(data);
+      });
+
+      // The client can also receive data from the server by reading from its socket.
+      client.on("data", (response) => {
+        // Check the readme for a simple explanation
+        var server_info = response.toString().split("\x00\x00");
+
         this.status = {
-            online: null,
-            version: null,
-            motd: null,
-            current_players: null,
-            max_players: null,
-            latency: null
+          host: this.host,
+          port: this.port,
+          status: true,
+          version: server_info[2].replace(/\u0000/g, ""),
+          motd: server_info[3].replace(/\u0000/g, ""),
+          current_players: server_info[4].replace(/\u0000/g, ""),
+          max_players: server_info[5].replace(/\u0000/g, ""),
+          latency: this.status.latency
         };
-    }
+        formatMotd(server_info[3]);
+        // Request an end to the connection after the data has been received.
+        client.end();
+        resolve(this.status);
+      });
 
-    getStatus() {
-        return new Promise((resolve, reject) => {
-            var start_time = new Date();
-            const client = net.connect(this.port, this.host, () => {
-                this.status.latency = Math.round(new Date() - start_time);
-                // 0xFE packet identifier for a server list ping
-                // 0x01 server list ping's payload (always 1)
-                let data = Buffer.from([0xfe, 0x01]);
-                client.write(data);
-            });
+      client.on("end", () => {
+        // console.log('Requested an end to the TCP connection');
+      });
 
-            // The client can also receive data from the server by reading from its socket.
-            client.on("data", (response) => {
-                // Check the readme for a simple explanation
-                var server_info = response.toString().split("\x00\x00");
+      client.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
 
-                this.status = {
-                    host: this.host,
-                    port: this.port,
-                    status: true,
-                    version: server_info[2].replace(/\u0000/g, ""),
-                    motd: server_info[3].replace(/\u0000/g, ""),
-                    current_players: server_info[4].replace(/\u0000/g, ""),
-                    max_players: server_info[5].replace(/\u0000/g, ""),
-                    latency: this.status.latency
-                };
-                formatMotd(server_info[3]);
-                // Request an end to the connection after the data has been received.
-                client.end();
-                resolve(this.status);
-            });
-
-            client.on("end", () => {
-                // console.log('Requested an end to the TCP connection');
-            });
-
-            client.on("error", (err) => {
-                reject(err);
-            });
-        });
-    }
-
-    async asyncStatus() {
-        let status = await this.getStatus();
-        return status;
-    }
+  async asyncStatus() {
+    let status = await this.getStatus();
+    return status;
+  }
 }
 
 function formatMotd(motd) {
-    let noSpaces = motd.replace(/\u0000/g, "");
-    Buffer.from(noSpaces);
-    // let noColor = noSpaces.toString().replace(/[^\x00-\x7F]/g, '');
-    // console.log(Buffer.from(motd, 'utf8').toString('hex'));
-    // console.log(noColor);
+  let noSpaces = motd.replace(/\u0000/g, "");
+  Buffer.from(noSpaces);
+  // let noColor = noSpaces.toString().replace(/[^\x00-\x7F]/g, '');
+  // console.log(Buffer.from(motd, 'utf8').toString('hex'));
+  // console.log(noColor);
 }
 
 function PingMCServer(ip, port, callback) {
-    new MCServStatus(port, ip)
-        .getStatus()
-        .then((res) => {
-            callback(res, null);
-        })
-        .catch((rej) => {
-            callback(null, rej);
-        });
+  new MCServStatus(port, ip)
+    .getStatus()
+    .then((res) => {
+      callback(res, null);
+    })
+    .catch((rej) => {
+      callback(null, rej);
+    });
 }
 
 // 任务对象缓存
@@ -96,54 +96,54 @@ const TASK_OBJECT_DATABASE = {};
 
 // 当服务器开启时，为其创建一个定时任务
 function CreateMCPingTask(id, ip, port) {
-    // 若任务存在，禁止重复创建，每个服务器有且只有一个定时查询任务
-    if (TASK_DATABASE[id]) {
-        return;
-    }
+  // 若任务存在，禁止重复创建，每个服务器有且只有一个定时查询任务
+  if (TASK_DATABASE[id]) {
+    return;
+  }
 
-    // 任务参数对象，用于记录错误次数和其他数据
-    TASK_OBJECT_DATABASE[id] = {
-        errorCount: 0
-    };
+  // 任务参数对象，用于记录错误次数和其他数据
+  TASK_OBJECT_DATABASE[id] = {
+    errorCount: 0
+  };
 
-    // 每隔 6 秒，ping 查询一次服务器状态，并且缓存结果
-    const taskInterval = setInterval(() => {
-        // 进行查询
-        PingMCServer(ip, port, (v, e) => {
-            if (v != null && e == null) {
-                // 查询成功则缓存值
-                MCPING_RESULT_DATABASE[id] = v;
-            } else {
-                // 连续查询错误次数 300 次以上，即 30 分钟，主动销毁自身
-                TASK_OBJECT_DATABASE[id] && TASK_OBJECT_DATABASE[id].errorCount++;
-                if (TASK_OBJECT_DATABASE[id] && TASK_OBJECT_DATABASE[id].errorCount > 300) {
-                    DestroyMCPingTask(id);
-                }
-            }
-        });
-    }, 1000 * 6);
-    // 记录定时任务
-    TASK_DATABASE[id] = taskInterval;
+  // 每隔 6 秒，ping 查询一次服务器状态，并且缓存结果
+  const taskInterval = setInterval(() => {
+    // 进行查询
+    PingMCServer(ip, port, (v, e) => {
+      if (v != null && e == null) {
+        // 查询成功则缓存值
+        MCPING_RESULT_DATABASE[id] = v;
+      } else {
+        // 连续查询错误次数 300 次以上，即 30 分钟，主动销毁自身
+        TASK_OBJECT_DATABASE[id] && TASK_OBJECT_DATABASE[id].errorCount++;
+        if (TASK_OBJECT_DATABASE[id] && TASK_OBJECT_DATABASE[id].errorCount > 300) {
+          DestroyMCPingTask(id);
+        }
+      }
+    });
+  }, 1000 * 6);
+  // 记录定时任务
+  TASK_DATABASE[id] = taskInterval;
 }
 
 // 当服务器关闭时，为其关闭一个定时任务
 function DestroyMCPingTask(id) {
-    // 清理任务与其缓存
-    clearInterval(TASK_DATABASE[id]);
-    TASK_DATABASE[id] = undefined;
-    MCPING_RESULT_DATABASE[id] = undefined;
-    TASK_OBJECT_DATABASE[id] = undefined;
+  // 清理任务与其缓存
+  clearInterval(TASK_DATABASE[id]);
+  TASK_DATABASE[id] = undefined;
+  MCPING_RESULT_DATABASE[id] = undefined;
+  TASK_OBJECT_DATABASE[id] = undefined;
 }
 
 function QueryMCPingTask(id) {
-    return MCPING_RESULT_DATABASE[id];
+  return MCPING_RESULT_DATABASE[id];
 }
 
 module.exports = {
-    PingMCServer,
-    CreateMCPingTask,
-    DestroyMCPingTask,
-    QueryMCPingTask
+  PingMCServer,
+  CreateMCPingTask,
+  DestroyMCPingTask,
+  QueryMCPingTask
 };
 
 // PROMISE VERSION
--- route/websocket/console/history.js
+++ route/websocket/console/history.js
@@ -7,62 +7,62 @@ const HISTORY_SIZE_LINE = 1024;
 
 // 正序历史记录路由
 WebSocketObserver().listener("server/console/history", (data) => {
-    let userName = data.WsSession.username;
-    let bodyJson = JSON.parse(data.body);
-    let serverName = bodyJson["serverName"] || "";
+  let userName = data.WsSession.username;
+  let bodyJson = JSON.parse(data.body);
+  let serverName = bodyJson["serverName"] || "";
 
-    if (permssion.isCanServer(userName, serverName)) {
-        const server = serverModel.ServerManager().getServer(serverName);
-        if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。")
-        const logHistory = server.logHistory;
-        if (!logHistory) {
-            response.wsSend(data.ws, "server/console/history", "terminalBack", "[控制面板]: 暂无任何历史记录.\r\n");
-            return;
-        }
-        logHistory.readLine(userName, HISTORY_SIZE_LINE, (sendText) => {
-            if (sendText) {
-                sendText = sendText.replace(/\n/gim, "\r\n");
-                sendText = sendText.replace(/\r\r\n/gim, "\r\n");
-                response.wsSend(data.ws, "server/console/history", "terminalBack", sendText);
-            } else {
-                response.wsSend(data.ws, "server/console/history", "terminalBack", "[控制面板]: 无法再读取更多的服务端日志.\r\n");
-            }
-        });
+  if (permssion.isCanServer(userName, serverName)) {
+    const server = serverModel.ServerManager().getServer(serverName);
+    if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。");
+    const logHistory = server.logHistory;
+    if (!logHistory) {
+      response.wsSend(data.ws, "server/console/history", "terminalBack", "[控制面板]: 暂无任何历史记录.\r\n");
+      return;
     }
+    logHistory.readLine(userName, HISTORY_SIZE_LINE, (sendText) => {
+      if (sendText) {
+        sendText = sendText.replace(/\n/gim, "\r\n");
+        sendText = sendText.replace(/\r\r\n/gim, "\r\n");
+        response.wsSend(data.ws, "server/console/history", "terminalBack", sendText);
+      } else {
+        response.wsSend(data.ws, "server/console/history", "terminalBack", "[控制面板]: 无法再读取更多的服务端日志.\r\n");
+      }
+    });
+  }
 });
 
 // 首次进入终端使用,倒序历史记录路由
 WebSocketObserver().listener("server/console/history_reverse", (data) => {
-    let userName = data.WsSession.username;
-    let bodyJson = JSON.parse(data.body);
-    let serverName = bodyJson["serverName"] || "";
+  let userName = data.WsSession.username;
+  let bodyJson = JSON.parse(data.body);
+  let serverName = bodyJson["serverName"] || "";
 
-    if (permssion.isCanServer(userName, serverName)) {
-        const server = serverModel.ServerManager().getServer(serverName);
-        if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。")
-        const logHistory = server.logHistory;
-        if (!logHistory) return;
-        logHistory.readLineOnce(userName, HISTORY_SIZE_LINE * 3, (sendText) => {
-            if (sendText) {
-                sendText = sendText.replace(/\n/gim, "\r\n");
-                sendText = sendText.replace(/\r\r\n/gim, "\r\n");
-                response.wsSend(data.ws, "server/console/history", "terminalBack", sendText);
-            }
-        });
-    }
+  if (permssion.isCanServer(userName, serverName)) {
+    const server = serverModel.ServerManager().getServer(serverName);
+    if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。");
+    const logHistory = server.logHistory;
+    if (!logHistory) return;
+    logHistory.readLineOnce(userName, HISTORY_SIZE_LINE * 3, (sendText) => {
+      if (sendText) {
+        sendText = sendText.replace(/\n/gim, "\r\n");
+        sendText = sendText.replace(/\r\r\n/gim, "\r\n");
+        response.wsSend(data.ws, "server/console/history", "terminalBack", sendText);
+      }
+    });
+  }
 });
 
 // 历史指针重置路由
 WebSocketObserver().listener("server/console/history_reset", (data) => {
-    let userName = data.WsSession.username;
-    let bodyJson = JSON.parse(data.body);
-    let serverName = bodyJson["serverName"] || "";
+  let userName = data.WsSession.username;
+  let bodyJson = JSON.parse(data.body);
+  let serverName = bodyJson["serverName"] || "";
 
-    if (permssion.isCanServer(userName, serverName)) {
-        const server = serverModel.ServerManager().getServer(serverName);
-        if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。")
-        const logHistory = server.logHistory;
-        if (!logHistory) return;
-        logHistory.setPoint(userName, 0);
-    }
+  if (permssion.isCanServer(userName, serverName)) {
+    const server = serverModel.ServerManager().getServer(serverName);
+    if (!server) throw new Error("查询某服务器日志历史时，此服务器并不存在。");
+    const logHistory = server.logHistory;
+    if (!logHistory) return;
+    logHistory.setPoint(userName, 0);
+  }
 });
