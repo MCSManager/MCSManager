e1a7553e601a38343223693757f171f8fe837cda
Suwings <suwings@outlook.com>
2022-02-04 13:41:59 +0800

    新增 兼容旧版的启动方式

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log(`______  _______________________  ___                                         
+___   |/  /_  ____/_  ___/__   |/  /_____ _____________ _______ _____________
+__  /|_/ /_  /    _____ \\__  /|_/ /_  __  /_  __ \\  __  /_  __  /  _ \\_  ___/
+_  /  / / / /___  ____/ /_  /  / / / /_/ /_  / / / /_/ /_  /_/ //  __/  /    
+/_/  /_/  \\____/  /____/ /_/  /_/  \\__,_/ /_/ /_/\\__,_/ _\\__, / \\___//_/     
+                                                        /____/             
+ + Released under the GPL-3.0 License
+ + https://github.com/MCSManager
+`);
+
+console.log(`The installation and startup method you are using is no longer supported,
+you may be reading an older version of the MCSManager tutorial or documentation.
+Please go to https://mcsmanager.com/ official website for the latest installation method.
+The program has automatically exited.
+`);
+
+console.log(`您使用的安装和启动方式已经不再支持，您可能正在阅读旧版本的 MCSManager 教程或文档。
+请前往 https://mcsmanager.com/ 官方网站了解最新安装方式。
+程序已自动退出。
+`);
+
+// 因大量教程与文章还停留在 MCSManager 8 阶段，所以必须对其旧教程做一定的适配工作。
