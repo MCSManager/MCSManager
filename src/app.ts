cbe4cf843ab4dfd747fb84a9465ded71a4d11809
Suwings <Suwings@outlook.com>
2022-03-29 22:11:36 +0800

    删除 koa-compress 依赖库

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,6 @@ import { v4 } from "uuid";
 import path from "path";
 import koaBody from "koa-body";
 import session from "koa-session";
-import compress from "koa-compress";
 import koaStatic from "koa-static";
 import http from "http";
 
@@ -110,12 +109,6 @@ app.use(protocolMiddleware);
 
 // 静态文件路由
 app.use(koaStatic(path.join(BASE_PATH, "public")));
-
-const options = {
-  threshold: 1024 //数据超过1kb时压缩
-};
-app.use(compress(options));
-/* gzip压缩配置 end */
 
 // 装载所有路由
 import { index } from "./app/index";
