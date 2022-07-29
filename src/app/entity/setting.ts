4232c4af2c1470bd4e9009fc66594d0267afe42e
Suwings <Suwings@outlook.com>
2022-07-29 14:55:14 +0800

    Refactor: copyright info

--- src/app/entity/setting.ts
+++ src/app/entity/setting.ts
@@ -1,15 +1,4 @@
-/*
-  Copyright (C) 2022 https://github.com/mcsmanager team.
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Affero General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  According to the AGPL, it is forbidden to delete all copyright notices, 
-  and if you modify the source code, you must open source the
-  modified source code.
-*/
+// Copyright (C) 2022 MCSManager Team <mcsmanager-dev@outlook.com>
 
 // @Entity
 export default class SystemConfig {
