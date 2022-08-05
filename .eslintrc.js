383cb598ccca6318d042aa19aa9357225f9fc263
Suwings <Suwings@outlook.com>
2022-08-05 16:10:27 +0800

    Refactor: code info

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// Copyright (C) 2022 MCSManager <mcsmanager-dev@outlook.com>
+
 module.exports = {
   env: {
     browser: true,
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
+// Copyright (C) 2022 MCSManager <mcsmanager-dev@outlook.com>
+
 /* eslint-disable no-undef */
-const path = require('path');
-const nodeExternals = require('webpack-node-externals')
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "production",
-  entry: './dist/app.js',
-  target: 'node',
+  entry: "./dist/app.js",
+  target: "node",
   externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'production'),
-  },
+    filename: "app.js",
+    path: path.resolve(__dirname, "production")
+  }
 };
