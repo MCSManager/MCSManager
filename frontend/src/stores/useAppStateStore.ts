4e38561ee5e7bbb539f272125b7e9bfa4022158e
unitwk <unitwk@outlook.com>
2023-08-21 23:23:20 +0800

    Feat: user info fetch

--- frontend/src/stores/useAppStateStore.ts
+++ frontend/src/stores/useAppStateStore.ts
@@ -2,15 +2,16 @@ import { reactive } from "vue";
 import { createGlobalState } from "@vueuse/core";
 import _ from "lodash";
 
+interface AppStateInfo {
+  userInfo: BaseUserInfo | null;
+}
+
 export const useAppStateStore = createGlobalState(() => {
-  const state = reactive<{
-    userInfo: BaseUserInfo | null;
-  }>({
+  const state: AppStateInfo = reactive<AppStateInfo>({
     userInfo: null
   });
 
-  const cloneState = () => {
-    if (!state) return null;
+  const cloneState = (): AppStateInfo => {
     const tmp = _.cloneDeep(state);
     return reactive(tmp);
   };
