6e2f849321324e91fc88ffa6272ce84210b474ed
Lazy <2211717435@qq.com>
2023-09-21 13:42:38 +0800

    Feat: add start counts

--- frontend/src/widgets/instance/BaseInfo.vue
+++ frontend/src/widgets/instance/BaseInfo.vue
@@ -43,7 +43,9 @@ onMounted(async () => {
       <a-typography-paragraph>
         {{ t("TXT_CODE_7ec9c59c") }}{{ instanceInfo?.config.nickname }}
       </a-typography-paragraph>
-      <a-typography-paragraph> {{ t("TXT_CODE_68831be6") }}{{ instanceTypeText }} </a-typography-paragraph>
+      <a-typography-paragraph>
+        {{ t("TXT_CODE_68831be6") }}{{ instanceTypeText }}
+      </a-typography-paragraph>
       <a-typography-paragraph>
         <span>{{ t("TXT_CODE_e70a8e24") }}</span>
         <span v-if="isRunning" class="color-success">
@@ -68,8 +70,13 @@ onMounted(async () => {
         {{ t("TXT_CODE_8b8e08a6") }}{{ instanceInfo?.config.createDatetime }}
       </a-typography-paragraph>
       <a-typography-paragraph>
+        {{ t("启动次数：") }}{{ instanceInfo?.started }}
+      </a-typography-paragraph>
+      <a-typography-paragraph>
         <span>{{ t("TXT_CODE_cec321b4") }}{{ instanceInfo?.config.oe.toUpperCase() }} </span>
-        <span class="ml-6">{{ t("TXT_CODE_400a4210") }}{{ instanceInfo?.config.ie.toUpperCase() }} </span>
+        <span class="ml-6">
+          {{ t("TXT_CODE_400a4210") }}{{ instanceInfo?.config.ie.toUpperCase() }}
+        </span>
       </a-typography-paragraph>
     </template>
   </CardPanel>
