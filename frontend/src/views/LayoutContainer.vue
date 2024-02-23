3cc56edb5af6e7e09938687c7577fd672e651ee2
unitwk <unitwk@outlook.com>
2024-02-23 10:51:31 +0800

    Del: transform action

--- frontend/src/views/LayoutContainer.vue
+++ frontend/src/views/LayoutContainer.vue
@@ -151,13 +151,13 @@ const showCardOperator = (card: ILayoutCard) => {
 
 @keyframes scaleAnimation {
   0% {
-    opacity: 0;
-    transform: scale(0.98);
+    opacity: 0.1;
+    // transform: scale(0.98);
   }
 
   100% {
     opacity: 1;
-    transform: scale(1);
+    // transform: scale(1);
   }
 }
 </style>
