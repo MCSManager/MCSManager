90cced1475156df991c373500f6bcabf5fcd5baf
unitwk <unitwk@outlook.com>
2024-03-12 20:05:48 +0800

    Feat: Change Version

--- install-dependents.sh
+++ install-dependents.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 npm install
-
 npm run preview-build
 
 cd daemon
--- npm-dev-macos.sh
+++ npm-dev-macos.sh
@@ -4,6 +4,6 @@ currentPath="$(pwd)"
 for action in "daemon" "panel" "frontend"
 do
     terminalCmd="tell app \"Terminal\" to do script \"cd $currentPath && npm run $action\""
-    echo "Run: $terminalCmd"
+    echo "Open New Terminal: $terminalCmd"
     osascript -e "$terminalCmd"
 done
