45cb00e87ba1bab3304e3dda8a04fdf4dfab2fe6
Unitwk <Unitwk@outlook.com>
2024-08-09 15:34:59 +0800

    Feat: exchange platform api

--- panel/src/app/routers/instance_exchange_router.ts
+++ panel/src/app/routers/instance_exchange_router.ts
@@ -4,6 +4,7 @@ import validator from "../middleware/validator";
 import { ROLE } from "../entity/user";
 import {
   buyOrRenewInstance,
+  getNodeStatus,
   queryInstanceByUserId,
   RequestAction
 } from "../service/exchange_service";
@@ -23,7 +24,7 @@ router.post(
       const requestAction = ctx.request.body.request_action;
       const params = ctx.request.body.data ?? {};
       if (requestAction === RequestAction.PING) {
-        ctx.body = "OK";
+        ctx.body = await getNodeStatus(params);
         return;
       }
       if ([RequestAction.RENEW, RequestAction.BUY].includes(requestAction)) {
--- panel/src/app/service/exchange_service.ts
+++ panel/src/app/service/exchange_service.ts
@@ -119,21 +119,49 @@ export async function queryInstanceByUserId(params: Record<string, any>) {
 
   const { instances = [] } = await getInstancesByUuid(user.uuid, true);
   const newInstancesInfo = instances.map((v) => {
+    const lines = [
+      {
+        title: t("TXT_CODE_f49149d0"),
+        value: v.docker?.ports
+      }
+    ];
+    if (v.info?.maxPlayers && Number(v.info?.maxPlayers) != -1) {
+      lines.push({
+        title: t("TXT_CODE_7e9727bd"),
+        value: `${v.info?.currentPlayers}/${v.info?.maxPlayers}`
+      });
+    }
     return {
       name: v.nickname,
       expire: v.endTime,
       status: v.status,
-      lines: [
-        {
-          title: t("TXT_CODE_7e9727bd"),
-          value: `${v.info?.currentPlayers}/${v.info?.maxPlayers}`
-        },
-        {
-          title: t("TXT_CODE_f49149d0"),
-          value: v.docker?.ports
-        }
-      ]
+      lines
     };
   });
   return newInstancesInfo;
+}
+
+export async function getNodeStatus(params: Record<string, any>) {
+  const nodeId = toText(params.node_id) ?? "";
+  const remoteService = RemoteServiceSubsystem.getInstance(nodeId);
+  if (!remoteService?.available) {
+    throw new Error(t("TXT_CODE_bed32084"));
+  }
+  const remoteRequest = new RemoteRequest(remoteService);
+  const remoteInfo = await remoteRequest.request("info/overview");
+  remoteInfo.uuid = remoteService.uuid;
+  remoteInfo.ip = remoteService.config.ip;
+  remoteInfo.port = remoteService.config.port;
+  remoteInfo.prefix = remoteService.config.prefix;
+  remoteInfo.available = remoteService.available;
+  remoteInfo.remarks = remoteService.config.remarks;
+  return {
+    name: String(remoteInfo.remarks),
+    id: String(remoteInfo.uuid),
+    ip: String(remoteInfo.ip),
+    port: Number(remoteInfo.port),
+    available: Boolean(remoteInfo.available),
+    running: Number(remoteInfo.instance.running),
+    instances: Number(remoteInfo.instance.total)
+  };
 }
