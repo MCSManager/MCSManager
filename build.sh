089f57eff34ae9924fa203be3eeae2b35f22bd47
YuMao <Unitwk@outlook.com>
2024-09-27 15:07:10 +0800

    chore: optimize build.sh

--- build.sh
+++ build.sh
@@ -32,19 +32,21 @@ mkdir production-code/web/public
 
 mv ./daemon/production/app.js ./production-code/daemon
 mv ./daemon/production/app.js.map ./production-code/daemon
-cp ./daemon/package.json ./production-code/daemon/package.json
-cp ./daemon/package-lock.json ./production-code/daemon/package-lock.json
+cp -f ./daemon/package.json ./production-code/daemon/package.json
+cp -f ./daemon/package-lock.json ./production-code/daemon/package-lock.json
 
 mv ./panel/production/app.js ./production-code/web
 mv ./panel/production/app.js.map ./production-code/web
-cp ./panel/package.json ./production-code/web/package.json
-cp ./panel/package-lock.json ./production-code/web/package-lock.json
+cp -f ./panel/package.json ./production-code/web/package.json
+cp -f ./panel/package-lock.json ./production-code/web/package-lock.json
 
 mv ./frontend/dist/* ./production-code/web/public
 
 rm -rf ./daemon/dist ./daemon/production
 rm -rf ./panel/dist ./panel/production
 rm -rf ./frontend/dist
+
+cp -r -f ./daemon/lib ./production-code/daemon/lib
 
 echo "------------"
 echo "Compilation completed!"
