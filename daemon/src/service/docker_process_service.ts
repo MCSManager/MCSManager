87c08cc922b5480c60adb8ac3fc0980f3d9961ec
YuMao <Unitwk@outlook.com>
2025-04-22 11:07:29 +0800

    refactor: mcsmanager-common layer

--- daemon/src/service/docker_process_service.ts
+++ daemon/src/service/docker_process_service.ts
@@ -100,7 +100,7 @@ export class SetupDockerContainer extends AsyncTask {
     let cpusetCpus: string | undefined = undefined;
     if (instance.config.docker.cpusetCpus) {
       const arr = instance.config.docker.cpusetCpus.split(",");
-      arr.forEach((v) => {
+      arr.forEach((v: string) => {
         if (isNaN(Number(v))) throw new Error($t("TXT_CODE_instance.invalidCpu", { v }));
       });
       cpusetCpus = instance.config.docker.cpusetCpus;
