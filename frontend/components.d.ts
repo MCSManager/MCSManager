5dcf8cdbd307f17ceca12c1babbc8735a2f402c0
yumao233 <unitwk@outlook.com>
2025-06-17 09:24:06 +0800

    feat: iframe component

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -78,6 +78,8 @@ declare module 'vue' {
     Empty: typeof import('./src/components/Empty.vue')['default']
     FadeUpAnimation: typeof import('./src/components/FadeUpAnimation.vue')['default']
     IconBtn: typeof import('./src/components/IconBtn.vue')['default']
+    IframeBox: typeof import('./src/components/IframeBox/index.vue')['default']
+    IframePro: typeof import('./src/components/IframePro.vue')['default']
     InnerCard: typeof import('./src/components/InnerCard.vue')['default']
     InputDialogProvider: typeof import('./src/components/InputDialogProvider.vue')['default']
     InstanceConfigEditor: typeof import('./src/components/InstanceConfigEditor.vue')['default']
