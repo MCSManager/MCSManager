716b65e88e7ca239c3655d756a1b93638ea7b2b6
Bluemangoo <chenfy2006@qq.com>
2025-07-03 15:57:23 +0800

    fix: take over running containers

--- daemon/src/entity/commands/docker/docker_takeover.ts
+++ daemon/src/entity/commands/docker/docker_takeover.ts
@@ -8,39 +8,34 @@ import {
 } from "../../../service/docker_process_service";
 import AbsStartCommand from "../start";
 import { DefaultDocker } from "../../../service/docker_service";
+import Docker from "dockerode";
 
 export default class DockerTakeoverCommand extends AbsStartCommand {
+  constructor(private containerInfo: Docker.ContainerInfo) {
+    super();
+  }
+
   protected async createProcess(instance: Instance) {
     // Docker docks to the process adapter
     const processAdapter = new DockerProcessAdapter(new SetupDockerContainer(instance));
     try {
       const docker = new DefaultDocker();
-      const containers = (await docker.listContainers())
-        .map((container) => {
-          const label = container.Labels["mcsmanager.instance.uuid"];
-          if (label == null || container.Status != "running") {
-            return null;
-          }
-          return {
-            uuid: label,
-            container: container
-          };
-        })
-        .filter((c) => c?.uuid == instance.instanceUuid);
-      if (containers.length > 0) {
-        await processAdapter.start(
-          {
-            isTty: instance.config.terminalOption.pty,
-            w: instance.config.terminalOption.ptyWindowCol,
-            h: instance.config.terminalOption.ptyWindowRow
-          },
-          docker.getContainer(containers[0]!.container.Id) // throw error if not found, and catch later
-        );
+      const container = docker.getContainer(this.containerInfo.Id);
+      if (this.containerInfo.State == "paused") {
+        await container.unpause();
       }
+      await processAdapter.start(
+        {
+          isTty: instance.config.terminalOption.pty,
+          w: instance.config.terminalOption.ptyWindowCol,
+          h: instance.config.terminalOption.ptyWindowRow
+        },
+        container
+      );
     } catch (e) {
       logger.error(e);
       logger.error("uuid:", instance.instanceUuid);
-      logger.error("container:", instance.instanceUuid);
+      logger.error("container:", this.containerInfo.Id);
       throw new StartupDockerProcessError($t("TXT_CODE_786c22bd"));
     }
 
--- daemon/src/service/takeover_container.ts
+++ daemon/src/service/takeover_container.ts
@@ -1,23 +1,27 @@
 import { DefaultDocker } from "./docker_service";
 import InstanceSubsystem from "./system_instance";
 import DockerTakeoverCommand from "../entity/commands/docker/docker_takeover";
+import { ContainerInfo } from "dockerode";
 
 export default async function takeoverContainer() {
   const docker = new DefaultDocker();
-  const uuids = (await docker.listContainers())
-    .map((container) => {
-      const label = container.Labels["mcsmanager.instance.uuid"];
-      if (label == null || container.Status != "running") {
-        return null;
-      }
-      return label;
-    })
-    .filter((c) => c != null);
+  const containers = new Map<string, ContainerInfo>();
+  (await docker.listContainers()).forEach((container) => {
+    const label = container.Labels["mcsmanager.instance.uuid"];
+    if (label == null) {
+      return;
+    }
+    if (containers.has(label)) {
+      return; // Skip if already exists
+    }
+    containers.set(label, container);
+  });
 
-  for (const uuid of uuids) {
-    const instance = InstanceSubsystem.getInstance(uuid!);
+  for (const c of containers) {
+    const [uuid, container] = c;
+    const instance = InstanceSubsystem.getInstance(uuid);
     if (instance && instance.config.processType == "docker") {
-      await instance.forceExec(new DockerTakeoverCommand());
+      await instance.forceExec(new DockerTakeoverCommand(container));
     }
   }
 }
