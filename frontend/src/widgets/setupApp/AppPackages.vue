d31ccd1494ef8b3d0fad11a26ef84dc0f4dffcc9
Lazy <2211717435@qq.com>
2025-09-26 23:46:14 +0800

    chore: i18n

--- frontend/src/widgets/setupApp/AppPackages.vue
+++ frontend/src/widgets/setupApp/AppPackages.vue
@@ -475,7 +475,7 @@ onMounted(() => {
                     class="download-button"
                     @click="searchForm.gameType = item.gameType"
                   >
-                    {{ t("展开") }}
+                    {{ t("TXT_CODE_5a936b7f") }}
                   </a-button>
                 </div>
               </div>
