b39fa200931b5769a8763c384824b3084a6c5df8
KagurazakaNyaa <dev@kagurazakanyaa.com>
2025-11-07 18:06:45 +0800

    fix: optimize cd command

--- prod-scripts/windows/start.bat
+++ prod-scripts/windows/start.bat
@@ -2,11 +2,11 @@ set BASE_DIR=%cd%
 set DAEMON_DIR="%BASE_DIR%\daemon"
 set WEB_DIR="%BASE_DIR%\web"
 if exist "%DAEMON_DIR%" (
-    cd %DAEMON_DIR%
+    cd /d "%DAEMON_DIR%"
     start node_app.exe --enable-source-maps --max-old-space-size=8192 app.js
 )
 ping localhost
 if exist "%WEB_DIR%" (
-    cd %WEB_DIR%
+    cd /d "%WEB_DIR%"
     start node_app.exe --enable-source-maps --max-old-space-size=8192 app.js --open
 )
