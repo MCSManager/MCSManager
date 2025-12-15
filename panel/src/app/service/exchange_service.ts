aa9c69b3b96e9a362792965b660b436fbbef42aa
yumao233 <unitwk@outlook.com>
2025-12-15 19:24:54 +0800

    feat: add online.mcsmanager.com

--- panel/src/app/service/exchange_service.ts
+++ panel/src/app/service/exchange_service.ts
@@ -12,8 +12,8 @@ import { getInstancesByUuid, IAdvancedInstanceInfo } from "./instance_service";
 // A commercial platform for selling instances released by the MCSManager Dev Team.
 // Currently, it only supports some countries and regions.
 // If you do not turn on "Commercial Mode", MCSManager will not send any data.
-// export const REDEEM_PLATFORM_ADDR = "https://redeem.mcsmanager.com";
-export const REDEEM_PLATFORM_ADDR = "https://redeem.mcsmanager.com";
+// export const REDEEM_PLATFORM_ADDR = "https://online.mcsmanager.com";
+export const REDEEM_PLATFORM_ADDR = "https://online.mcsmanager.com";
 
 // ------- Protocol Define -------
 
