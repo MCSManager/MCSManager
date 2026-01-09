404378c9bc72d921bb07aff13d60cc4acd5b18dd
SMGoro <72185434+SMGoro@users.noreply.github.com>
2026-01-09 18:34:41 +0800

    feat: add disk space limit input and update related translations

--- frontend/src/widgets/instance/dialogs/InstanceDetail.vue
+++ frontend/src/widgets/instance/dialogs/InstanceDetail.vue
@@ -857,6 +857,24 @@ defineExpose({
                   />
                 </a-form-item>
               </a-col>
+              <a-col :xs="24" :lg="4" :offset="0">
+                <a-form-item>
+                  <a-typography-title :level="5">{{ t("TXT_CODE_disk_limit") }}</a-typography-title>
+                  <a-typography-paragraph>
+                    <a-tooltip :title="t('TXT_CODE_disk_limit_desc')" placement="top">
+                      <a-typography-text type="secondary" class="typography-text-ellipsis">
+                        {{ t("TXT_CODE_disk_limit_desc") }}
+                      </a-typography-text>
+                    </a-tooltip>
+                  </a-typography-paragraph>
+                  <a-input
+                    v-model:value="formData.instance.config.docker.maxSpace"
+                    :allow-clear="true"
+                    :placeholder="t('TXT_CODE_disk_limit_placeholder')"
+                    type="number"
+                  />
+                </a-form-item>
+              </a-col>
               <a-col :xs="24" :lg="6" :offset="0">
                 <a-form-item>
                   <a-typography-title :level="5" class="require-field">
@@ -881,7 +899,7 @@ defineExpose({
                   </a-select>
                 </a-form-item>
               </a-col>
-              <a-col :xs="24" :lg="16" :offset="0">
+              <a-col :xs="24" :lg="12" :offset="0">
                 <a-form-item>
                   <a-typography-title :level="5">{{ t("TXT_CODE_fffaeb17") }}</a-typography-title>
                   <a-typography-paragraph>
@@ -1299,25 +1317,6 @@ defineExpose({
                     v-model:value="formData.instance.config.docker.memorySwappiness"
                     :allow-clear="true"
                     :placeholder="t('TXT_CODE_6f1129fb')"
-                  />
-                </a-form-item>
-              </a-col>
-
-              <a-col :xs="24" :lg="8" :offset="0">
-                <a-form-item>
-                  <a-typography-title :level="5">{{ t("TXT_CODE_disk_limit") }}</a-typography-title>
-                  <a-typography-paragraph>
-                    <a-tooltip :title="t('TXT_CODE_disk_limit_desc')" placement="top">
-                      <a-typography-text type="secondary" class="typography-text-ellipsis">
-                        {{ t("TXT_CODE_disk_limit_desc") }}
-                      </a-typography-text>
-                    </a-tooltip>
-                  </a-typography-paragraph>
-                  <a-input
-                    v-model:value="formData.instance.config.docker.maxSpace"
-                    :allow-clear="true"
-                    :placeholder="t('TXT_CODE_disk_limit_placeholder')"
-                    type="number"
                   />
                 </a-form-item>
               </a-col>
