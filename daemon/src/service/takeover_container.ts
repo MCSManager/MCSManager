d4d978edc82bd432093221434376129ce0de0d4c
yumao233 <unitwk@outlook.com>
2026-03-16 11:12:25 +0800

    feat: kill container when takeover docker container error

--- daemon/src/service/takeover_container.ts
+++ daemon/src/service/takeover_container.ts
@@ -1,27 +1,40 @@
+import { ContainerInfo } from "dockerode";
+import DockerTakeoverCommand from "../entity/commands/docker/docker_takeover";
 import { DefaultDocker } from "./docker_service";
 import InstanceSubsystem from "./system_instance";
-import DockerTakeoverCommand from "../entity/commands/docker/docker_takeover";
-import { ContainerInfo } from "dockerode";
+
+async function killContainer(containerInfo: ContainerInfo) {
+  const docker = new DefaultDocker();
+  const container = docker.getContainer(containerInfo.Id);
+  container.kill().catch(() => {});
+  container.remove().catch(() => {});
+}
 
 export default async function takeoverContainer() {
   const docker = new DefaultDocker();
   const containers = new Map<string, ContainerInfo>();
-  (await docker.listContainers()).forEach((container) => {
-    const label = container.Labels["mcsmanager.instance.uuid"];
+  (await docker.listContainers()).forEach((containerInfo) => {
+    const label = containerInfo.Labels["mcsmanager.instance.uuid"];
     if (label == null) {
       return;
     }
     if (containers.has(label)) {
       return; // Skip if already exists
     }
-    containers.set(label, container);
+    containers.set(label, containerInfo);
   });
 
-  for (const c of containers) {
-    const [uuid, container] = c;
+  for (const item of containers) {
+    const [uuid, container] = item;
     const instance = InstanceSubsystem.getInstance(uuid);
-    if (instance && instance.config.processType == "docker") {
-      await instance.forceExec(new DockerTakeoverCommand(container));
+    try {
+      if (instance && instance.config.processType == "docker") {
+        await instance.forceExec(new DockerTakeoverCommand(container));
+      }
+    } catch (e) {
+      docker.getContainer(container.Id).kill();
+      killContainer(container);
+      throw e;
     }
   }
 }
