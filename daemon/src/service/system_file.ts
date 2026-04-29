cac5c7874daa4829c4f995c5fbe89cc63f9445c2
xiaosu <xiaosu@xiaosu.icu>
2026-04-29 21:43:18 +0800

    fix: improve path normalization handling for absolute paths

--- daemon/src/service/system_file.ts
+++ daemon/src/service/system_file.ts
@@ -45,12 +45,12 @@ export default class FileManager {
     const topAbsolutePath = this.topPath;
 
     let finalPath = "";
-    if (os.platform() === "win32") {
+    if (path.isAbsolute(fileName)) {
+      finalPath = path.normalize(fileName);
+    } else if (os.platform() === "win32") {
       const reg = new RegExp("^[A-Za-z]{1}:[\\\\/]{1}");
       if (reg.test(this.cwd)) {
         finalPath = path.normalize(path.join(this.cwd, fileName));
-      } else if (reg.test(fileName)) {
-        finalPath = path.normalize(fileName);
       }
     }
 
